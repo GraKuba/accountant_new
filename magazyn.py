da2c2d2f226ed8838a9df24f74dbd6f526ca6024
Jakub <grabarczyk.kuba@gmail.com>
2022-04-05 18:38:33 +0200

    magazyn.py added

---
+++ magazyn.py
@@ -0,0 +1,7 @@
+from accountant import *
+downloading_history_form_file()
+adding_to_history_from_terminal()
+working_on_the_data()
+
+for object in warehouse:
+    print(f"Name: {object} | Amount: {warehouse[object]}")
